00bc75adead436c4a068c49486e746aa38ca9635
Lismond Bernard <lismond@acst.com>
2020-01-03 10:54:43 -0500

    SPM dependencies

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "DTFoundation",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "DTFoundation",
+            targets: ["DTFoundation"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "",
+            dependencies: []),
+    ]
+)
